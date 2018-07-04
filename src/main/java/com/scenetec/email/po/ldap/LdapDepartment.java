69247f81a55aa0a509749a77ee9fcb92f1399012
sqh1991129 <sqh1991129@163.com>
2018-07-04 15:30:26 +0800

    同步企业邮箱代码

---
+++ src/main/java/com/scenetec/email/po/ldap/LdapDepartment.java
@@ -0,0 +1,61 @@
+package com.scenetec.email.po.ldap;
+
+import java.util.Objects;
+
+public class LdapDepartment {
+
+    private String name;
+
+    private String parentName;
+    
+    private String parentId;
+
+    private boolean needAdd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LdapDepartment that = (LdapDepartment) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(parentName, that.parentName);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, parentName);
+    }
+
+    public boolean isNeedAdd() {
+        return needAdd;
+    }
+
+    public void setNeedAdd(boolean needAdd) {
+        this.needAdd = needAdd;
+    }
+}
